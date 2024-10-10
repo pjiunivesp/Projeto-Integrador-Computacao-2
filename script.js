6ecd488699ec6d6df85a89ad570028b59cadfdf6
liviatech <li.livia.tech@gmail.com>
2024-10-09 21:39:02 -0300

    revisão do codigo html e css

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
 
-// SCRIPT CARROCEU
-
-let currentIndex = 0;
-
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-slide');
-    if (index >= slides.length) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = slides.length - 1;
-    } else {
-        currentIndex = index;
-    }
-
-    const carouselImages = document.querySelector('.carousel-images');
-    const offset = -currentIndex * 750;
-    carouselImages.style.transform = `translateX(${offset}px)`;
-}
-
-function nextSlide() {
-    showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-    showSlide(currentIndex - 1);
-}
-
-// Mostrar o primeiro slide inicialmente
-showSlide(currentIndex);
 
 
 // SCRIPT AGENDA
